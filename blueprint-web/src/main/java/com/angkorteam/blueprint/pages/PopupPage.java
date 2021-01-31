6f1d0c959d954baa8c7a7bc1cb1c4a622b981b85
peter <peter_us@gamil.com>
2021-01-31 15:40:38 +0700

    dialog

--- blueprint-web/src/main/java/com/angkorteam/blueprint/pages/PopupPage.java
+++ blueprint-web/src/main/java/com/angkorteam/blueprint/pages/PopupPage.java
@@ -2,7 +2,7 @@ package com.angkorteam.blueprint.pages;
 
 import com.angkorteam.blueprint.MasterPage;
 import com.angkorteam.webui.frmk.common.Bookmark;
-import com.angkorteam.webui.frmk.wicket.extensions.ajax.markup.html.modal.ModalDialog;
+import com.angkorteam.webui.frmk.wicket.layout.ModalDialog;
 import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
@@ -15,8 +15,6 @@ public class PopupPage extends MasterPage {
     protected Form<Void> form;
 
     private AjaxButton buttonShow;
-
-    private ModalDialog modalDialog;
 
     @Override
     public void renderHead(IHeaderResponse response) {
@@ -35,10 +33,6 @@ public class PopupPage extends MasterPage {
             }
         };
         this.form.add(this.buttonShow);
-
-        this.modalDialog = new ModalDialog("modal");
-        this.modalDialog.trapFocus();
-        this.modalDialog.closeOnEscape();
 
         body.add(this.modalDialog);
 
--- blueprint-web/src/main/java/com/angkorteam/blueprint/pages/PopupPageDialog.java
+++ blueprint-web/src/main/java/com/angkorteam/blueprint/pages/PopupPageDialog.java
@@ -1,8 +1,13 @@
 package com.angkorteam.blueprint.pages;
 
+import com.angkorteam.webui.frmk.wicket.layout.MasterPage;
 import com.angkorteam.webui.frmk.wicket.markup.html.panel.Panel;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 
 public class PopupPageDialog extends Panel {
+
+    private AjaxLink<Void> okayButton;
 
     public PopupPageDialog(String id) {
         super(id);
@@ -14,6 +19,13 @@ public class PopupPageDialog extends Panel {
 
     @Override
     protected void onInitHtml() {
+        this.okayButton = new AjaxLink<Void>("okayButton") {
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                ((MasterPage) getPage()).getModalDialog().close(target);
+            }
+        };
+        add(this.okayButton);
     }
 
 }
